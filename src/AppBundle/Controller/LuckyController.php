485192325b9f06613d9e3d36db2b69c6cb348309
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2015-11-18 20:49:58 -0800

    Add api/lucky/number

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,5 +1,4 @@
 <?php
-// http://localhost:8888/webDev/symfony/isHosting/web/app_dev.php/lucky/number
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -11,12 +10,30 @@ class LuckyController extends Controller
     /**
      * @Route("/lucky/number")
      */
+    // http://localhost:8888/webDev/symfony/isHosting/web/app_dev.php/lucky/number
     public function numberAction()
     {
       $number = rand(0, 100);
 
       return new Response(
         '<html><body>Lucky number: '.$number.'</body></html>'
-    );
+      );
+    }
+
+    /**
+     * @Route("/api/lucky/number")
+     */
+    // http://localhost:8888/webDev/symfony/isHosting/web/app_dev.php/api/lucky/number
+    public function apiNumberAction()
+    {
+      $data = array(
+        'lucky_number' => rand(0, 100),
+      );
+
+      return new Response(
+        json_encode($data),
+        200,
+        array('Content-Type' => 'application/json')
+      );
     }
 }
